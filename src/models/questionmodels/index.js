59d551d9b7b806f44feb9e63d99c2e083bf62b94
Camila Nepomuceno <camila.cno@gmail.com>
2024-03-16 15:07:49 -0300

    Add index to question models

---
+++ src/models/questionmodels/index.js
@@ -0,0 +1,15 @@
+import { linkSchema, linkValidationSchema } from './linkSchema.js'
+import { optionSchema, optionValidationSchema } from './optionSchema.js'
+import { feedbackSchema, feedbackValidationSchema } from './feedbackSchema.js'
+import { Question, questionValidationSchema } from './questionSchema.js'
+
+export default {
+  linkSchema,
+  linkValidationSchema,
+  optionSchema,
+  optionValidationSchema,
+  feedbackSchema,
+  feedbackValidationSchema,
+  Question,
+  questionValidationSchema,
+}
